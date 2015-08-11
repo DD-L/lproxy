709d892847109083ead410f9020cc770245f5f59
Deel <deel@d-l.top>
2015-08-11 23:28:08 +0800

    added - boost::date_time

---
+++ src/core/boost_built/dateprebuilt.cpp
@@ -0,0 +1,7 @@
+#define BOOST_DATE_TIME_SOURCE
+
+#include <libs/date_time/src/gregorian/greg_names.hpp>
+#include <libs/date_time/src/gregorian/date_generators.cpp>
+#include <libs/date_time/src/gregorian/greg_month.cpp>
+#include <libs/date_time/src/gregorian/greg_weekday.cpp>
+#include <libs/date_time/src/gregorian/gregorian_types.cpp>
