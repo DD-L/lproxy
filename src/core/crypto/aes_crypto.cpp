367fa13fe95b5c59cb692ef4342113df1eb3e829
Deel <deel@d-l.top>
2016-01-17 00:56:36 +0800

    bugfix: aes

--- src/core/crypto/aes_crypto.cpp
+++ src/core/crypto/aes_crypto.cpp
@@ -7,134 +7,82 @@
 #include "crypto/aes_crypto.h"
 #include <assert.h>
 #include <type_traits>
-#include <cryptopp/hex.h>
-#ifndef CRYPTOPP_ENABLE_NAMESPACE_WEAK
-#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
-#endif
-#include <cryptopp/md5.h>
+
+#include "crypto/md5_crypto.h" // for md5
+#include <cryptopp/cryptlib.h> 
+// for CryptoPP::AutoSeededRandomPool
+// for CryptoPP::RandomNumberGenerator
+
+
+//#include <cryptopp/hex.h>
+//#ifndef CRYPTOPP_ENABLE_NAMESPACE_WEAK
+//#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+//#endif
+//#include <cryptopp/md5.h>
 using namespace crypto;
-Aes::Aes(const uint8_t* _key, size_t _key_len) {
+
+
+
+// http://cryptopp.com/wiki/Advanced_Encryption_Standard
+// https://cryptopp.com/wiki/CFB_Mode
+Aes::Aes(const uint8_t* _key, size_t _key_len) : md5_key(0x00, md5_key_len) {
     assert(_key);
     std::string key = (const char*)_key;
     assert(key.length() == _key_len);
-    make_md5key(key);
-    aesEncryptor.SetKey(md5_key, md5_key_len);
-    aesDecryptor.SetKey(md5_key, md5_key_len);
+
+    md5_key.Assign(&(make_md5key(key)[0]), md5_key_len);
+
+    initialize_cryptor();
 }
 
-Aes::Aes(const std::string& _key) {
-    make_md5key(_key);
-    aesEncryptor.SetKey(md5_key, md5_key_len);
-    aesDecryptor.SetKey(md5_key, md5_key_len);
+
+Aes::Aes(const std::string& _key) 
+    : md5_key(&(make_md5key(_key)[0]), md5_key_len) {
+
+    initialize_cryptor();
 }
 
-void Aes::make_md5key(const std::string& _key) {
-    CryptoPP::Weak::MD5 hash;
-    byte digest[ CryptoPP::Weak::MD5::DIGESTSIZE ]; // 16
-    hash.CalculateDigest( digest, (byte*) _key.c_str(), _key.length() );
+void Aes::initialize_cryptor(void) {
+    
+    //CryptoPP::AutoSeededRandomPool rnd;
+    CryptoPP::RandomNumberGenerator rnd;
+    // Generate a random IV
+    byte iv[CryptoPP::AES::BLOCKSIZE];
+    rnd.GenerateBlock(iv, CryptoPP::AES::BLOCKSIZE);
 
-    CryptoPP::HexEncoder encoder;
-    std::string output;
-    encoder.Attach( new CryptoPP::StringSink( output ) );
-    encoder.Put( digest, sizeof(digest) );
-    encoder.MessageEnd();
-
-    md5_key = (const uint8_t*)output.c_str();
-    assert(output.length() == md5_key_len);
+    aesEncryptor.SetKeyWithIV(md5_key, md5_key.size(), iv);
+    aesDecryptor.SetKeyWithIV(md5_key, md5_key.size(), iv);
 }
 
-/*
-uint8_t* Aes::encrypt(uint8_t* dest, const uint8_t* src, size_t src_len) {
-    this->execute(this->aesEncryptor, dest, src, src_len);
-    return dest;
+std::vector<uint8_t> Aes::make_md5key(const std::string& _key) {
+    std::vector<uint8_t> md5_cipher;
+    auto&& md5_encryptor = Md5();
+    md5_encryptor.encrypt(md5_cipher, 
+            (const uint8_t*)_key.c_str(), _key.size());
+    assert(md5_cipher.size() == md5_key_len);
+    return md5_cipher;
 }
-uint8_t* Aes::decrypt(uint8_t* dest, const uint8_t* src, size_t src_len) {
-    this->execute(this->aesDecryptor, dest, src, src_len);
-    return dest;
-}
 
-template<typename _aes_cryptor>
-void Aes::execute(_aes_cryptor& cryptor, 
-        uint8_t* dest, const uint8_t* src, size_t src_len) {
-
-    static_assert(std::is_same<_aes_cryptor, CryptoPP::AESEncryption>::value 
-            or std::is_same<_aes_cryptor, CryptoPP::AESDecryption>::value, 
-            "the type '_aes_cryptor' must be one of 'CryptoPP::AESEncryption',"
-            "'CryptoPP::AESDecryption'.");
-
-    byte xorBlock[CryptoPP::AES::BLOCKSIZE] = {0};
-    byte inBlock[CryptoPP::AES::BLOCKSIZE] = {0};
-    byte outBlock[CryptoPP::AES::BLOCKSIZE] = {0};
-
-    memset(xorBlock, 0, CryptoPP::AES::BLOCKSIZE);
-
-    const size_t block_size = CryptoPP::AES::BLOCKSIZE;
-    size_t       offset     = CryptoPP::AES::BLOCKSIZE;
-
-    const size_t max_loop = ((src_len % block_size) == 0) 
-        ? src_len / block_size 
-        : size_t((float)src_len / (float)block_size + 1.0f);
-
-    for (size_t loop = 0; loop < max_loop; ++loop) {
-
-        const size_t unproc_data = src_len - loop * block_size;
-        offset = (unproc_data >= block_size) ? block_size : unproc_data;
-
-        memset(inBlock, 0, block_size);
-        memmove(inBlock, (src + loop * offset), offset);
-
-        cryptor.ProcessAndXorBlock(inBlock, xorBlock, outBlock);
-
-        memmove((dest + loop * offset), outBlock, offset);
-    }
-}
-*/
 
 
 std::vector<uint8_t>& Aes::encrypt(std::vector<uint8_t>& dest, 
         const uint8_t* src, size_t src_len) {
-    this->execute(this->aesEncryptor, dest, src, src_len);
+    //this->execute(this->aesEncryptor, dest, src, src_len);
+    assert(src);
+    uint8_t output[src_len];
+    memset(output, 0 , src_len);
+    aesEncryptor.ProcessData(output, src, src_len);
+    dest.assign(output, output + src_len);
     return dest;
 }
 std::vector<uint8_t>& Aes::decrypt(std::vector<uint8_t>& dest, 
         const uint8_t* src, size_t src_len) {
-    this->execute(this->aesDecryptor, dest, src, src_len);
+    //this->execute(this->aesDecryptor, dest, src, src_len);
+    assert(src);
+    uint8_t output[src_len];
+    memset(output, 0 , src_len);
+    aesDecryptor.ProcessData(output, src, src_len);
+    dest.assign(output, output + src_len);
     return dest;
 }
 
-template<typename _aes_cryptor>
-void Aes::execute(_aes_cryptor& cryptor, 
-        std::vector<uint8_t>& dest, const uint8_t* src, size_t src_len) {
-
-    static_assert(std::is_same<_aes_cryptor, CryptoPP::AESEncryption>::value 
-            or std::is_same<_aes_cryptor, CryptoPP::AESDecryption>::value, 
-            "the type '_aes_cryptor' must be one of 'CryptoPP::AESEncryption',"
-            "'CryptoPP::AESDecryption'.");
-
-    byte xorBlock[CryptoPP::AES::BLOCKSIZE] = {0};
-    byte inBlock[CryptoPP::AES::BLOCKSIZE] = {0};
-    byte outBlock[CryptoPP::AES::BLOCKSIZE] = {0};
-
-    memset(xorBlock, 0, CryptoPP::AES::BLOCKSIZE);
-    dest.clear();
-
-    const size_t block_size = CryptoPP::AES::BLOCKSIZE;
-    size_t       offset     = CryptoPP::AES::BLOCKSIZE;
-
-    const size_t max_loop = ((src_len % block_size) == 0) 
-        ? src_len / block_size 
-        : size_t((float)src_len / (float)block_size + 1.0f);
-
-    for (size_t loop = 0; loop < max_loop; ++loop) {
-
-        const size_t unproc_data = src_len - loop * block_size;
-        offset = (unproc_data >= block_size) ? block_size : unproc_data;
-
-        memset(inBlock, 0, block_size);
-        memmove(inBlock, (src + loop * offset), offset);
-
-        cryptor.ProcessAndXorBlock(inBlock, xorBlock, outBlock);
-
-        dest.insert(dest.end(), outBlock, outBlock + offset);
-    }
-}
--- src/core/crypto/aes_crypto.h
+++ src/core/crypto/aes_crypto.h
@@ -9,6 +9,8 @@
 #include <string>
 #include "crypto/encryptor.h"
 #include <cryptopp/aes.h>
+#include <cryptopp/modes.h>    // for CryptoPP::CFB_Mode<AES>
+
 namespace crypto {
 
 class Aes : public Encrypt {
@@ -18,7 +20,7 @@ static_assert(CryptoPP::AES::BLOCKSIZE > 0,
 
 public:
     Aes(const uint8_t* _key, size_t _key_len);
-    Aes(const std::string& _key);
+    explicit Aes(const std::string& _key);
     Aes(const Aes&) = delete;
     Aes& operator= (const Aes&) = delete;
 
@@ -33,15 +35,16 @@ public:
     virtual std::vector<uint8_t>& decrypt(std::vector<uint8_t>& dest, 
                         const uint8_t* src, size_t src_len);
 private:
-    void make_md5key(const std::string& key);
-    template<typename _aes_cryptor>
-    void execute(_aes_cryptor& cryptor, 
-            std::vector<uint8_t>& dest, const uint8_t* src, size_t src_len);
+    void initialize_cryptor(void);
+    std::vector<uint8_t> make_md5key(const std::string& key);
 private:
-    const uint8_t*          md5_key;
+    
+    CryptoPP::SecByteBlock  md5_key;
     static const size_t     md5_key_len = 32;
-    CryptoPP::AESEncryption aesEncryptor; 
-    CryptoPP::AESDecryption aesDecryptor;
+
+    // CFB mode
+    CryptoPP::CFB_Mode<CryptoPP::AES>::Encryption aesEncryptor;
+    CryptoPP::CFB_Mode<CryptoPP::AES>::Decryption aesDecryptor;
  
 };
 
