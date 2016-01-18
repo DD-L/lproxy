45e6f1fedf981c3e382df44b82e5ccffabbb9f70
Deel <deel@d-l.top>
2016-01-18 15:50:41 +0800

    RFC2045: The encoded output stream must be represented in lines of no more than 76 characters each...

--- src/core/crypto/base64_crypto.h
+++ src/core/crypto/base64_crypto.h
@@ -16,12 +16,19 @@ class Base64 : public Encrypt {
 public:
     Base64(const Base64&) = delete;
     Base64& operator= (const Base64&) = delete;
-    Base64(void);
+    // LF 编码输出中是否允许包含换行符, 默认不允许包含换行符
+    // true: 编码输出包含换行符; false: 编码输出包含换行符  
+    Base64(bool allow_LF = false);
 
     virtual std::vector<uint8_t>& encrypt(std::vector<uint8_t>& dest, 
             const uint8_t* src, size_t src_len);
     virtual std::vector<uint8_t>& decrypt(std::vector<uint8_t>& dest, 
             const uint8_t* src, size_t src_len);
+private:
+    // RFC2045 标准规定:
+    // The encoded output stream must be represented in lines of no more
+    // than 76 characters each.
+    bool LF; // true: 编码输出包含换行符; false: 编码输出包含换行符  
 }; // class Base64
 
 } // namespace crypto
