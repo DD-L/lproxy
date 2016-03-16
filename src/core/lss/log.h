f6773f26d601b514c57c6b9fdc213aa13c7fcb91
Deel <deel@d-l.top>
2016-03-16 15:12:31 +0800

    logoutput2

--- src/core/lss/log.h
+++ src/core/lss/log.h
@@ -18,12 +18,23 @@ enum Which { LOCAL = 0, SERVER = 1 };
 
 void output_thread(Which which) {
     // 如果输出目标被多个日志线程访问，需在编译选项加上 -DLOG_USE_LOCK
+#ifndef LOGOUTPUT2
     auto& logoutput = LogOutput_t::get_instance();
+#else
+    auto& logoutput = LogOutput2_t::get_instance();
+#endif // if not define(LOGOUTPUT2)
 
+#ifndef LOGOUTPUT2
     // 日志输出到std::cout;
-    // 只输出日志权重大于等于TRACE的日志;
+    // 默认输出权重 大于等于 TRACE 级别的日志;
     // 日志输出格式采用默认格式
     logoutput.bind(std::cout);
+#else
+    // 日志输出到std::cout;
+    // 默认只输出 TRACE DEBUG INFO WARN ERROR FATAL 级别的日志;
+    // 日志输出格式采用默认格式
+    logoutput.bind(std::cout);
+#endif // if not define(LOGOUTPUT2)
 
     sdata_t logfilename = "";
     switch (which) {
@@ -41,9 +52,15 @@ void output_thread(Which which) {
     if (logfilename != "") {
         logfile.open(logfilename, std::ofstream::app);
         assert(logfile);
-        // 只输出日志权重大于等于ERROR的日志
+#ifndef LOGOUTPUT2
+        // 只输出 ERROR FATAL 的日志
         // 日志输出格式采用默认格式
         logoutput.bind(logfile, makelevel(ERROR));
+#else
+        // 只输出权重大于等于 ERROR  级别的日志
+        // 日志输出格式采用默认格式
+        logoutput.bind(logfile, { makelevel(ERROR), makelevel(FATAL) });
+#endif // if not define(LOGOUTPUT2)
     }
 
     std::shared_ptr<LogVal> val = std::make_shared<LogVal>();
@@ -52,7 +69,11 @@ void output_thread(Which which) {
         // TODO
         // do something
     } 
+#ifndef LOGOUTPUT2
     logoutput.unbind(std::cout);
+#else
+    logoutput.unbind(std::cout);
+#endif // if not define(LOGOUTPUT2)
 }
 } // namespace lproxy::log
 } // namespace lproxy
