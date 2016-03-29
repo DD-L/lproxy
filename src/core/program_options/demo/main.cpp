b71a3c4c57fc637e5e49e9db372ac19d3448a1da
Deel <deel@d-l.top>
2016-03-29 19:35:36 +0800

    feature program-options

---
+++ src/core/program_options/demo/main.cpp
@@ -0,0 +1,96 @@
+/*************************************************************************
+	> File Name:    main.cpp
+	> Author:       D_L
+	> Mail:         deel@d-l.top
+	> Created Time: 2016/3/29 9:22:11
+ ************************************************************************/
+
+#include <iostream>
+#include "../program_options.h"
+
+void process_program_options(const program_options& po) {
+    if (po.empty()) {
+        std::string message = po.show_help("arg empty");
+        std::cout << message;
+        exit(0);
+    }
+    if (po.count("-h") || po.count("--help")) {
+        std::string message = po.show_help("this is title");
+        std::cout << message;
+        exit(0);
+    }
+    if (po.count("-v") || po.count("--version")) {
+        std::cout << "ver: 0.1" << std::endl;
+        exit(0);
+    }
+    try {
+        if (po.count("-p")) {
+            std::string value = po.get("-p");
+            std::cout << value << std::endl;
+            return;
+        }
+        if (po.count("--print")) {
+            std::string value = po.get("--print");
+            std::cout << value << std::endl;
+            return;
+        }
+    }
+    catch (const program_options::parameter_error& error) {
+        std::cerr << error.what() << std::endl;
+        exit(-1);
+    }
+
+
+    std::cerr << "Unsupported options" << std::endl;
+    exit(1);
+}
+
+int main(int argc, char* argv[]) {
+    program_options po("demo [option]");
+    po.add_option("-h, --help", "Show this message.");
+    po.add_option("-v, --version", "Show current version.");
+    po.add_option("-p, --print", "Print a message.\ne.g. demo -p hello");
+    po.store(argc, argv);
+
+    process_program_options(po);
+
+    return 0;
+}
+
+/*
+
+$ make clean; make
+
+$ ./bin/demo
+arg empty
+Usage: demo [option]
+
+Options:
+  -h, --help     Show this message.
+  -v, --version  Show current version.
+  -p, --print    Print a message.
+                 e.g. demo -p hello
+
+$ ./bin/demo -h
+this is title
+Usage: demo [option]
+
+Options:
+  -h, --help     Show this message.
+  -v, --version  Show current version.
+  -p, --print    Print a message.
+                 e.g. demo -p hello
+
+$ ./bin/demo --version
+ver: 0.1
+
+$ ./bin/demo -p helloworld
+helloworld
+
+$ ./bin/demo -p
+Exception: program_options::parameter_error: -p
+
+$ ./bin/demo -xxxxx
+Unsupported options
+
+*/
---
+++ src/core/program_options/program_options.h
@@ -0,0 +1,122 @@
+#ifndef _PROGRAM_OPTIONS_H_1
+#define _PROGRAM_OPTIONS_H_1
+/*************************************************************************
+	> File Name:    program_options.h
+	> Author:       D_L
+	> Mail:         deel@d-l.top
+	> Created Time: 2016/3/29 9:06:26
+ ************************************************************************/
+
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <vector>
+//#include <set>
+#include <cstddef>
+#include <sstream>
+#include <assert.h>
+#include <exception>
+
+class program_options {
+public:
+    class parameter_error : public std::exception {
+    public:
+        parameter_error(const std::string& option) noexcept 
+            : m_msg("Exception: program_options::parameter_error: " + option) {}
+        virtual ~parameter_error (void) noexcept {}
+        virtual const char* what() const noexcept {
+            return m_msg.c_str();
+        }
+    private:
+        std::string m_msg;
+    };
+public:
+    explicit program_options(const std::string& usage) : m_usage(usage) {}
+    void add_option(const std::string& options, const std::string& message) {
+        m_max_options_str_length = std::max(m_max_options_str_length, 
+                options.length());
+        m_helpmessage.push_back({options, message});
+
+        // 分割 options  (", "),  并存放到 m_opts_set 中去, 
+        // 以便验证 经store 的参数是否符合要求.
+        // TODO
+    }
+
+    const std::string show_help(const std::string& title = "") const {
+        std::ostringstream message;
+        if (title != "") {
+            message << title << std::endl;
+        }
+        message << "Usage: " << m_usage << std::endl << std::endl;
+        message << "Options: " << std::endl;
+
+        for (auto& v : m_helpmessage) {
+            // 分割 v.help, 分割符'\n'
+            std::vector<std::string> helplines;
+            std::istringstream iss(v.help);
+            std::string line;
+            while (getline(iss, line, '\n')) {
+                helplines.push_back(line);
+            }
+            assert(helplines.size() >= 1);
+            message << "  " << std::left << std::setw(m_max_options_str_length)
+                <<  v.options << "  "  << helplines[0] << std::endl;
+            for (auto cit = helplines.cbegin() +1; 
+                    cit != helplines.cend(); ++cit) {
+                message << "  " //<< std::left 
+                    << std::setw(m_max_options_str_length) << ' ' << "  "
+                    << *cit << std::endl;
+            }
+        }
+        return message.str();
+    }
+
+    void store(int argc, char** argv) {
+        m_argc = argc; 
+        for (int i = 0; i < argc; ++i) {
+            m_argv.push_back(argv[i]);
+        }
+    }
+
+    bool count(const std::string& option) const {
+        auto ret = std::find(m_argv.begin(), m_argv.end(), option);
+        return ret != m_argv.end();
+    }
+
+    bool empty(void) const {
+        // *unix
+        // TODO
+        return m_argc == 1;
+    }
+
+    std::string get(const std::string& option) const {
+        // 在 argv 中找 option
+        for (auto cit = m_argv.cbegin(); cit != m_argv.cend(); ++cit) {
+            if (option == *cit) {
+                auto ret = cit + 1;
+                if (ret != m_argv.cend()) {
+                    return *(cit+1); 
+                }
+                else {
+                    // 参数错误
+                    throw parameter_error(option); 
+                }
+            }
+        }
+        // 没有找到
+        return "";
+    }
+
+private:
+    struct opts_help_t {
+        std::string options;
+        std::string help;
+    };
+    std::string    m_usage;
+    std::size_t    m_max_options_str_length = 0;
+    std::vector<opts_help_t> m_helpmessage;
+    int                      m_argc;
+    std::vector<std::string> m_argv;
+};
+
+#endif // _PROGRAM_OPTIONS_H_1
