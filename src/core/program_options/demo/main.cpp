bda8ff256f1a3100f0da38af194d893466313a5e
Deel <deel@d-l.top>
2016-04-01 09:44:58 +0800

    bugfix

--- src/core/program_options/demo/main.cpp
+++ src/core/program_options/demo/main.cpp
@@ -6,69 +6,84 @@
  ************************************************************************/
 
 #include <iostream>
+#include <assert.h>
 #include "../program_options.h"
 
-void process_program_options(const program_options& po) {
-    if (po.empty()) {
-        std::string message = po.show_help("arg empty");
+void process_program_options(const program_options* po) {
+    assert(po);
+    if (po->empty()) {
+        std::string message = po->show_help("arg empty");
         std::cout << message;
+
+        delete po; po = nullptr;
         exit(0);
     }
-    if (po.count("-h") || po.count("--help")) {
-        std::string message = po.show_help("this is title");
+    if (po->count("-h") || po->count("--help")) {
+        std::string message = po->show_help("this is title");
         std::cout << message;
+
+        delete po; po = nullptr;
         exit(0);
     }
-    if (po.count("-v") || po.count("--version")) {
+    if (po->count("-v") || po->count("--version")) {
         std::cout << "ver: 0.1" << std::endl;
+
+        delete po; po = nullptr;
         exit(0);
     }
-    if (po.count("-c") || po.count("--command")) {
-        for (auto& v : po.get_argv()) {
+    if (po->count("-c") || po->count("--command")) {
+        for (auto& v : po->get_argv()) {
             std::cout << v << " ";
         }
         std::cout << std::endl;
+
+        delete po; po = nullptr;
         exit(0);
     }
     try {
-        if (po.count("-p")) {
-            std::string value = po.get("-p");
+        if (po->count("-p")) {
+            std::string value = po->get("-p");
             std::cout << value << std::endl;
             return;
         }
-        if (po.count("--print")) {
-            std::string value = po.get("--print");
+        if (po->count("--print")) {
+            std::string value = po->get("--print");
             std::cout << value << std::endl;
             return;
         }
     }
     catch (const program_options::parameter_error& error) {
         std::cerr << error.what() << std::endl;
+        
+        delete po; po = nullptr;
         exit(-1);
     }
 
 
     std::cerr << "Unsupported options" << std::endl;
+
+    delete po; po = nullptr;
     exit(1);
 }
 
 int main(int argc, char* argv[]) {
-    program_options* po_ptr = new program_options("demo [option]");
-    po_ptr->add_option("-h, --help", "Show this message.");
-    po_ptr->add_option("-v, --version", "Show current version.");
-    po_ptr->add_option("-p, --print", "Print a message.\ne.g. demo -p hello");
-    po_ptr->add_option("-c, --command", "Print current command.");
+    program_options* po = new program_options("demo [option]");
+    po->add_option("-h, --help", "Show this message.");
+    po->add_option("-v, --version", "Show current version.");
+    po->add_option("-p, --print   <something>", "Print a message.\ne.g. demo -p hello");
+    po->add_option("-c, --command", "Print current command.");
 
-    po_ptr->example("demo --help");
-    po_ptr->example("demo -v");
-    po_ptr->example("demo -p helloworld");
-    po_ptr->example("demo -c");
+    po->example("demo --help");
+    po->example("demo -v");
+    po->example("demo -p helloworld");
+    po->example("demo -c");
 
-    po_ptr->store(argc, argv);
+    po->store(argc, argv);
 
-    process_program_options(*po_ptr);
+    process_program_options(po);
+    delete po; po = nullptr;
 
-    delete po_ptr;
+    // do something
     return 0;
 }
 
