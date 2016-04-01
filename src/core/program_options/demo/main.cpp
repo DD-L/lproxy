d793809a1ae83be593265a4e752b267a6f0d5ee3
Deel <deel@d-l.top>
2016-04-01 10:38:20 +0800

    fix

--- src/core/program_options/demo/main.cpp
+++ src/core/program_options/demo/main.cpp
@@ -7,92 +7,83 @@
 
 #include <iostream>
 #include <assert.h>
+#include <cstdlib> // for exit()
 #include "../program_options.h"
 
-void process_program_options(const program_options* po) {
-    assert(po);
-    if (po->empty()) {
-        std::string message = po->show_help("arg empty");
+void process_program_options(const program_options& po
+        /*, get_option*/) {
+    if (po.empty()) {
+        std::string message = po.show_help("arg empty");
         std::cout << message;
-
-        delete po; po = nullptr;
         exit(0);
     }
-    if (po->count("-h") || po->count("--help")) {
-        std::string message = po->show_help("this is title");
+    if (po.count("-h") || po.count("--help")) {
+        std::string message = po.show_help("this is title");
         std::cout << message;
-
-        delete po; po = nullptr;
         exit(0);
     }
-    if (po->count("-v") || po->count("--version")) {
+    if (po.count("-v") || po.count("--version")) {
         std::cout << "ver: 0.1" << std::endl;
-
-        delete po; po = nullptr;
         exit(0);
     }
     /*
-    if (po->count("-s") || po->count("--sizeof-this")) {
+    if (po.count("-s") || po.count("--sizeof-this")) {
         std::cout << "sizeof (program_options) = " << sizeof (program_options)
             << std::endl;
         std::cout << "sizeof (int) = " << sizeof (int) << std::endl;
         std::cout << "sizeof (void*) = " << sizeof (void*) << std::endl;
-
-        delete po; po = nullptr;
         exit(0);
     }
     */
-    if (po->count("-c") || po->count("--command")) {
-        for (auto& v : po->get_argv()) {
+    if (po.count("-c") || po.count("--command")) {
+        for (auto& v : po.get_argv()) {
             std::cout << v << " ";
         }
         std::cout << std::endl;
-
-        delete po; po = nullptr;
         exit(0);
     }
     try {
-        if (po->count("-p")) {
-            std::string value = po->get("-p");
+        if (po.count("-p")) {
+            std::string value = po.get("-p");
             std::cout << value << std::endl;
             return;
         }
-        if (po->count("--print")) {
-            std::string value = po->get("--print");
+        if (po.count("--print")) {
+            std::string value = po.get("--print");
             std::cout << value << std::endl;
             return;
         }
     }
     catch (const program_options::parameter_error& error) {
         std::cerr << error.what() << std::endl;
-        
-        delete po; po = nullptr;
         exit(-1);
     }
 
     std::cerr << "Unsupported options" << std::endl;
-
-    delete po; po = nullptr;
     exit(1);
 }
 
+void option(int argc, char** argv /*, get_option*/) {
+    assert(argv);
+    program_options po("demo [option]");
+    po.add_option("-h, --help", "Show this message.");
+    po.add_option("-v, --version", "Show current version.");
+    po.add_option("-p, --print   <something>", "Print a message.\ne.g. demo -p hello");
+    po.add_option("-c, --command", "Print current command.");
+    //po.add_option("-s, --sizeof-this", "printf(\"%d\", sizeof (program_options));");
+
+    po.example("demo --help");
+    po.example("demo -v");
+    po.example("demo -p helloworld");
+    po.example("demo -c");
+
+    po.store(argc, argv);
+
+    process_program_options(po /*, get_option*/);
+}
+
 int main(int argc, char* argv[]) {
-    program_options* po = new program_options("demo [option]");
-    po->add_option("-h, --help", "Show this message.");
-    po->add_option("-v, --version", "Show current version.");
-    po->add_option("-p, --print   <something>", "Print a message.\ne.g. demo -p hello");
-    po->add_option("-c, --command", "Print current command.");
-    //po->add_option("-s, --sizeof-this", "printf(\"%d\", sizeof (program_options));");
-
-    po->example("demo --help");
-    po->example("demo -v");
-    po->example("demo -p helloworld");
-    po->example("demo -c");
-
-    po->store(argc, argv);
-
-    process_program_options(po);
-    delete po; po = nullptr;
+    option(argc, argv /*, get_option*/);
 
     // do something
     return 0;
