cb5e8d5634c7ee5131c8f2dac8cb671eb6464fa5
Deel <deel@d-l.top>
2016-04-04 19:26:12 +0800

    language

--- src/core/language/langCN.hpp
+++ src/core/language/langCN.hpp
@@ -1,7 +1,12 @@
-﻿// 注意此文件的编码格式
+﻿#ifndef _LANGCN_HPP_1_
+#define _LANGCN_HPP_1_
+// 注意此文件的编码格式
+
+#include <language/lang.h>
 struct LangCN: Lang {
 	LangCN(void) {
 		m["hello"] = "你好";
 		m["hehe"] = "呵呵";
 	}
 };
+#endif //_LANGCN_HPP_1_
--- src/core/language/langEN.hpp
+++ src/core/language/langEN.hpp
@@ -1,7 +1,12 @@
-﻿struct LangEN: Lang {
+﻿#ifndef _LANGEN_HPP_1_
+#define _LANGEN_HPP_1_
+
+#include <language/lang.h>
+struct LangEN: Lang {
 	LangEN(void) {
 		m["hello"] = "hello";
 		m["hehe"] = "hehe";
 	}
 };
+#endif //_LANGEN_HPP_1_
 
